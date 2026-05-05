1fc9c21ebef1c3ae2a1c29c0c3f3850acb3c37b7
oreocool1 <158286169+oreocool1@users.noreply.github.com>
2026-05-05 10:26:32 +0100

    Update saving test.py

--- saving test.py
+++ saving test.py
@@ -10,7 +10,12 @@
 #if os.path.exists("test.txt"):
 #    print("file exsists")
 #else:
-#    open("test.txt","x")
+#with open("test.txt","x") as test:
+ #   ()
+# test.close()
+test = open("test.txt" , "w")
+test.write("this is a test\n")
+test.write("this is a test\n")
 #    print("file created")
 #f = open("demofile.txt")
 #test = f.readlines()
