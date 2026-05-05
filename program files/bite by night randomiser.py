e63db15f01bbce4a65336ad278035abe987acf1a
oreocool1 <158286169+oreocool1@users.noreply.github.com>
2026-05-05 14:54:36 +0100

    simplified V2 Ready for player data update

--- program files/bite by night randomiser.py
+++ program files/bite by night randomiser.py
@@ -14,27 +14,28 @@ print ('''
 update = datetime.datetime(2026 , 5 , 30)
 hint = datetime.datetime(2026 , 5 , 10)
 currentDate = datetime.datetime.now()
-if os.path.exists("playerData.txt"):
-       with open("playerData.txt" , "r") as playerDataFile:
-              playerData = playerDataFile.readlines()
-       print("Player Data loaded...")
-       time.sleep(1)
-else:
-       print("player data not found")
-       with open("playerData.txt", "x") as playerDataFile:
-             ()
-       playerDataFile = open("playerData.txt" , "w")
-       print("Hi i cant find your player data this means we have to create one please answer these quick questions")
-       tempSpring = str(input("Do you have Springtrap| please say 'yes' or 'no'"))
-       tempmimic = str(input("do you have the mimic|please say 'yes' or 'no'"))
-       tempenared = str(input("do you have enared | please say 'yes' or 'no' "))
-       tempSpring = tempSpring.lower()
-       tempmimic = tempmimic.lower()
-       tempenared = tempenared.lower()
-       playerDataFile.writelines("spring" + tempSpring + os.linesep)
-       playerDataFile.writelines("mimic" + tempmimic + os.linesep)
-       playerDataFile.writelines("eneee" + tempenared + os.linesep)
-       playerDataFile.close()
+# if os.path.exists("playerData.txt"):
+#        with open("playerData.txt" , "r") as playerDataFile:
+#               playerData = playerDataFile.read()
+#        print("Player Data loaded...")
+#        print(playerData)
+#        time.sleep(1)
+# else:
+#        print("player data not found")
+#        with open("playerData.txt", "x") as playerDataFile:
+#              ()
+#        playerDataFile = open("playerData.txt" , "w")
+#        print("Hi i cant find your player data this means we have to create one please answer these quick questions")
+#        tempSpring = str(input("Do you have Springtrap| please say 'yes' or 'no'"))
+#        tempmimic = str(input("do you have the mimic|please say 'yes' or 'no'"))
+#        tempenared = str(input("do you have enared | please say 'yes' or 'no' "))
+#        tempSpring = tempSpring.lower()
+#        tempmimic = tempmimic.lower()
+#        tempenared = tempenared.lower()
+#        playerDataFile.writelines("spring" + tempSpring + os.linesep)
+#        playerDataFile.writelines("mimic" + tempmimic + os.linesep)
+#        playerDataFile.writelines("eneee" + tempenared + os.linesep)
+#        playerDataFile.close()
 if currentDate > update:
        print ("please update version https://github.com/oreocool1/bite-by-night-randomiser")
        end = input("click enter to exit")
@@ -43,20 +44,22 @@ elif currentDate > hint:
        print ("please update version https://github.com/oreocool1/bite-by-night-randomiser")
        print ("resuming program")
        time.sleep(1)
-temp = input ("do you want to delete your player DATA? (yes or no)")
-temp = temp.lower()
-if temp == "yes":
-       if os.path.exists("playerData.txt"):
-              os.remove("playerData.txt")
-              quit()
-       else:
-              print("ERROR NO FILE EXSITS")
-              quit()
+# temp = input ("do you want to delete your player DATA? (yes or no)")
+# temp = temp.lower()
+# if temp == "yes":
+#        if os.path.exists("playerData.txt"):
+#               os.remove("playerData.txt")
+#               print("file deleted exiting")
+#               time.sleep(1)
+#               quit()
+#        else:
+#               print("ERROR NO FILE EXSITS")
+#               quit()
 while True:
-       char = input("do you need killer 'k', surviver's' or both 'b' ")
-       char = char.lower()
+       char = input("select enter to choose a killer") #("do you need killer 'k', surviver's' or both 'b' ")
+       #char = char.lower()
        correct = False
-       if char == "k" or char == "b":
+       if 2+2 == 4:#char == "k" or char == "b":
               while correct == False:
                      num = random.randint(1,3) #random.randint(1,4)
                      if num == 1:
@@ -64,14 +67,14 @@ while True:
                             correct = True
                      elif num == 2:
                             print ("the mimic")
-                            correct == True
+                            correct = True
                      elif num == 3:
                             print ("enared")
-                            correct ==True
+                            correct = True
                      # elif num == 4:
                      #        print ("puppet")
                      else:
                             print("Error found retrying ...")
                             time.sleep(0.1)
        else:
-              print("Error please enter K (killer) as S (surviver is unter development) ")
+              print("Error please enter valid data")
