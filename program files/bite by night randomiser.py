eb25b58a397f67ff50e4ccd69439284b431e3fa0
oreocool1 <158286169+oreocool1@users.noreply.github.com>
2026-05-05 10:09:27 +0100

    added preparations for player data

--- program files/bite by night randomiser.py
+++ program files/bite by night randomiser.py
@@ -1,21 +1,33 @@
 import random
 import time
 import datetime
+import os
 print ('''
        🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪
        🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪
        🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪
-       🟪 Welcome to Bite by night randomiser V1 🟪
+       🟪 Welcome to Bite by night randomiser V2 🟪
        🟪🟪🟪🟪🟪🟪🟪 this is a wip  🟪🟪🟪🟪🟪🟪🟪
        🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪
        🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪
        ''')
 update = datetime.datetime(2026 , 5 , 30)
+hint = datetime.datetime(2026 , 5 , 10)
 currentDate = datetime.datetime.now()
+if os.path.exists("playerData.txt"):
+       print("Player Data loaded...")
+       time.sleep(1)
+else:
+       print("player data not found")
+       playerData = open("playerData.txt", "x")
 if currentDate > update:
        print ("please update version https://github.com/oreocool1/bite-by-night-randomiser")
        end = input("click enter to exit")
        quit()
+elif currentDate > hint:
+       print ("please update version https://github.com/oreocool1/bite-by-night-randomiser")
+       print ("resuming program")
+       time.sleep(1)
 while 2+2 == 4:
        char = input("do you need killer 'k', surviver's' or both 'b' ")
        char = char.lower()
@@ -37,7 +49,6 @@ while 2+2 == 4:
                             print ("the mimic")
                             correct == True
                      elif num == 3:
-                            if place == "f" or place == "m":
                                    print ("enared")
                                    correct ==True
                                    #temp = random.randint(1,2)
